ddcc6ee952a0315e0971edabd903094ef13a0a17
Imukua <ianmukuaa@gmail.com>
2023-05-11 07:00:27 -0700

    fix 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,15 +5,13 @@ from sys import argv
 def argprint():
     num = len(argv) - 1
     if num == 1:
-        print("1 argument:\n1: {}".format(argv))
+        print("1 argument:\n1: {}".format(argv[1]))
     elif num == 0:
         print("0 arguments.")
     else:
-        print("{} arguments:\n".format(num))
-        count = 1
+        print("{} arguments:".format(num))
         for i in range(num):
-            print("{}: {}\n".format(count, argv[count]))
-            count += 1
+            print("{}: {}".format(i + 1, argv[i + 1]))
 
 
 if __name__ == "__main__":
