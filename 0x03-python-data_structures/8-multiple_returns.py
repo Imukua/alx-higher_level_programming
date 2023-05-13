60dce09ec97a8c2b4dc4709a2acf652a32d2eb78
Imukua <ianmukuaa@gmail.com>
2023-05-13 07:31:20 -0700

    fix 9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if senetence == "":
-        return(0,None)
-    slen = len(sentence)
-    firstchar = sentence[0]
-    return(slen, firstchar)
+        return(0, None)
+    return(len(sentence),sentence[0])
