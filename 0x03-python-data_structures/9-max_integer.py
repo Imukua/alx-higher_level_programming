bd78fc3c77b48e5c38783a99ca1581033845586d
Imukua <ianmukuaa@gmail.com>
2023-05-13 07:36:06 -0700

    fix 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if not len(my_list):
-        return(none)
+        return(None)
     a = my_list[0]
     for item in my_list:
         if item > a:
