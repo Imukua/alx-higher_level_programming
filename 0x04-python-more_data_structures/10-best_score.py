e12c4887f88bfc0eb988b15245e7ca40c7c02f58
Imukua <ianmukuaa@gmail.com>
2023-05-17 09:28:35 -0700

    fix 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(list(a_dictionary))
+    max_int = max(a_dictionary, key=a_dictionary.get)
+    return max_int
