8b63b4925f437888d6a201d6c6149a109737e06e
Imukua <ianmukuaa@gmail.com>
2023-08-14 18:05:08 +0300

    implemented task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const firstArg = process.argv[2];
+const intArg = parseInt(firstArg);
+
+if (!isNaN(intArg)){
+    console.log(`My number: ${intArg}`)
+} else{
+    console.log("Not a number")
+}
