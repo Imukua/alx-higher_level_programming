78827912bd70b19285cdd3afdf1b5bd722a0cd23
Imukua <ianmukuaa@gmail.com>
2023-08-14 18:29:38 +0300

    implemented task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const numTimes = process.argv[2];
+let intValue = parseInt(numTimes);
+const charPrint = "C is fun";
+if (!isNaN(intValue)){
+    while (intValue > 0){
+        console.log(charPrint);
+        intValue--;
+    }
+}else{
+    console.log("Missing number of occurences")
+}
