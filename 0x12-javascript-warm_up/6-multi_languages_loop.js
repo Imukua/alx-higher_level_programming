782f2f54750a5ebc2ee2da4488ebf711a6400d9a
Imukua <ianmukuaa@gmail.com>
2023-08-15 02:24:08 +0300

    fixed spelling

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const strArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const strArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const sentense of strArray) {
   console.log(sentense);
 }
