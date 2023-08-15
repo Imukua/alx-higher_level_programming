f105c5cb79249083e6dfd231cb84113022b34058
Imukua <ianmukuaa@gmail.com>
2023-08-15 11:34:06 +0300

    added X as print char

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 
 const Size = process.argv[2];
 const intValue = parseInt(Size);
-const charPrint = '#';
+const charPrint = 'X';
 if (!isNaN(intValue)) {
   for (let i = 0; i < intValue; i++) {
     console.log(charPrint.repeat(Size));
