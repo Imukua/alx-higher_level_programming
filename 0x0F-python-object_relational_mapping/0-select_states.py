491471fbf096cccd1554900fe7707d8616ee7c2d
Imukua <ianmukuaa@gmail.com>
2023-08-19 14:54:01 +0300

    added ineractivity check

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,16 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(username = sys.argv[2],
-                           password = sys.argv[2],
-                           db = sys.argv[3],
-                           port = 3306,
-                           host = 'localhost')
+if __name__ - '__main__':
+    db = MySQLdb.connect(username=sys.argv[2],
+                         password=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306,
+                         host='localhost')
 
-cur = db.cursor()
-query = 'SELECT * FROM states'
-cur.execute(query)
-statesList = [row[0] for row in cur.fetchall()]
-for state in statesList:
-    print(state)
+    cur = db.cursor()
+    query = 'SELECT * FROM states'
+    cur.execute(query)
+    statesList = [row[0] for row in cur.fetchall()]
+    for state in statesList:
+        print(state)
