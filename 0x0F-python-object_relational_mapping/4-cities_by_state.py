fc203b13d785f60d4db98fa32b410e34053ab9af
Imukua <ianmukuaa@gmail.com>
2023-08-20 13:05:06 +0300

    implemented task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
 
-def main():
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
+    )
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, password=password, db=database)
     cursor = db.cursor()
 
-    query = """
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    INNER JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id ASC;
-    """
+    cursor.execute(
+        """SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states
+        ON states.id = cities.state_id
+        ORDER BY cities.id"""
+    )
 
-    cursor.execute(query)
+    rows = cursor.fetchall()
 
-    for row in cursor.fetchall():
+    for row in rows:
         print(row)
 
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    main()
