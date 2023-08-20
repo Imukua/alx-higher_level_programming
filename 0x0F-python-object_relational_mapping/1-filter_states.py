59a3aa0aba5c9beb6901787abc7b8af6c0cbf8a3
Imukua <ianmukuaa@gmail.com>
2023-08-20 12:17:12 +0300

    IMLEMENTED TASK 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,23 +8,19 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
     db = MySQLdb.connect(
-        user=username,
-        password=password,
-        db=database,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
         port=3306,
         host='localhost'
     )
-    
+
     query = '''
         SELECT * 
         FROM states 
-        WHERE name LIKE "N%" 
-        ORDER BY id ASC
+        WHERE name LIKE BINARY "N%" 
+        ORDER BY states.id ASC
     '''
     cur = db.cursor()
     cur.execute(query)
