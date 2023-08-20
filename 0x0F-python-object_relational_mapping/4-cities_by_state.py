2d63df00686de2b451a4c13c84a8c854a287e3ee
Imukua <ianmukuaa@gmail.com>
2023-08-20 13:01:57 +0300

    fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,7 @@ def main():
     database = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
+                         user=username, password=password, db=database)
     cursor = db.cursor()
 
     query = """
