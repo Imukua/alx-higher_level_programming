ed0d15604f035e3a231de2c8863781a7c7377281
Imukua <ianmukuaa@gmail.com>
2023-08-20 13:59:59 +0300

    fx

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -6,6 +6,11 @@ from sqlalchemy.orm import sessionmaker
 from model_state import State
 
 if __name__ == "__main__":
+    """
+    Query using SQL alchemy
+
+    """
+
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
