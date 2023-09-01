10d193f41a24da984f9aa151c0df8d0b1024d8ca
Imukua <ianmukuaa@gmail.com>
2023-09-01 20:37:14 +0300

    made the, executable

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 """
-Sends a POST request to a specified URL with an email parameter.
+Sends a request to a given URL and displays the response body.
 
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
 """
 
 import sys
-import urllib.parse
+import urllib.error
 import urllib.request
 
+
 if __name__ == "__main__":
-    # Get the URL and email from command-line arguments
     target_url = sys.argv[1]
-    email_value = {"email": sys.argv[2]}
 
-    # Encode the email parameter as ASCII
-    encoded_data = urllib.parse.urlencode(email_value).encode("ascii")
-
-    # Create a POST request with the encoded data
-    request = urllib.request.Request(target_url, encoded_data)
-
-    # Send the request and print the response body
-    with urllib.request.urlopen(request) as response:
-        response_body = response.read().decode("utf-8")
-        print(response_body)
+    request = urllib.request.Request(target_url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
