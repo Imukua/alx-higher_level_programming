3915bfa4d8d8e5be66bc48cd927e93a3db447df4
Imukua <ianmukuaa@gmail.com>
2023-09-01 20:55:48 +0300

    implemented taks 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status using the requests package.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for HTTP status codes >= 400
+
+        print("Body response:")
+        print("\t- type:", type(response.text))
+        print("\t- content:", response.text)
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
