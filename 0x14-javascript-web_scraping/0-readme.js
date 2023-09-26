15d32adcbf2f11c40e7cce976a9c3281012d037d
Imukua <ianmukuaa@gmail.com>
2023-09-26 23:17:42 +0300

    made file executable

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-//load the module
-const fs = require("fs");
+// load the module
+const fs = require('fs');
 // read the file
-fs.readFile(process.argv[2], "utf8", function (error, content) {
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
   console.log(error || content);
 });
