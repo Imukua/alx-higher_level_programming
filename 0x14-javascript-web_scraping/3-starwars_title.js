51347bf8b6e83972dd882c4107a8c4e115a899a0
Imukua <ianmukuaa@gmail.com>
2023-09-27 01:23:58 +0300

    semistandard fixes

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 // set url
-let urls = 'http://swapi.co/api/films/' + process.argv[2];
+const urls = 'http://swapi.co/api/films/' + process.argv[2];
 request(urls, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
