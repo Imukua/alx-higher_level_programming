d884890868d6ac53406ba821633284dd33968b5c
Imukua <ianmukuaa@gmail.com>
2023-09-27 01:08:30 +0300

    Semistandard updates

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-//send get request
+// send get request
 request.get(process.argv[2]).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });
